4c084c2309753b9eb662117c1b73c038a8539975
Abigail Einhorn <abbyeinhorn@gmail.com>
2018-12-24 10:57:15 +0200

    added dist folder

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express()
 app.use('/', api)
 
 
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+
 const port = 4500
 app.listen(port, function(){
     console.log(`Running on port ${port}`)
